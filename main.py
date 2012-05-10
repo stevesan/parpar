3fe4282d257bc4bfbbde6dd4ff912b26cac2580c
Steven An <stevenan@gmail.com>
2012-05-10 02:43:44 -0400

    md5 scores; daily scores

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from datetime import datetime, timedelta
 
+import md5
+
+SECRET_MD5_SALT = "iluvhorsebeatoff"
+
 class MyRequestHandler( webapp.RequestHandler ):
 	def output(self,s): self.response.out.write(s)
 
@@ -10,7 +14,9 @@ class ScoreEntry( db.Model ):
 	player = db.StringProperty( multiline=False )
 	song = db.StringProperty( multiline=False )
 	value = db.IntegerProperty()
-	when = db.DateTimeProperty(auto_now_add=True)
+	timestamp = db.DateTimeProperty( auto_now_add=True )
+	# Store this separately for efficient equality daily queries
+	date = db.DateProperty( auto_now_add=True )
 
 	def get_value(self): return self.value
 
@@ -24,46 +30,45 @@ class SaveScore( MyRequestHandler ):
 			songName = self.request.get('song')
 			playerName = self.request.get('player')
 			value = int(self.request.get('value'))
+			given_hexdigest = self.request.get('hexdigest')
 
-			entry = ScoreEntry()
-			entry.song = songName
-			entry.player = playerName
-			entry.value = value
-			entry.put()
-
-			self.output('score = %d saved!' % value)
+			# check the digest
+			expected_hexdigest = md5.new('%s%s%d%s' % (songName, playerName, value, SECRET_MD5_SALT)).hexdigest()
+			if expected_hexdigest != given_hexdigest:
+				self.output('bad MD5 hexdigest given. Score not saved. Given = '+given_hexdigest)
+			else:
+				entry = ScoreEntry()
+				entry.song = songName
+				entry.player = playerName
+				entry.value = value
+				entry.put()
+				self.output('score = %d saved!' % value)
 	
 class GetScores( MyRequestHandler ):
 	def get(self):
 		self.response.headers['Content-Type'] = 'text/plain'
 		song = self.request.get('song')
 		limit = int(self.request.get('limit'))
-		daysago = int(self.request.get('days'))
+		justtoday = int(self.request.get('justtoday'))
 
-		dateClause = ''
 		results = None
-		if daysago > 0:
+
+		if justtoday == 1:
 			# enforce time limit
-			dt = timedelta(1)
-			now = datetime.today()
-			minWhen = now-dt
 			results = db.GqlQuery('SELECT player, value FROM ScoreEntry '
-				'WHERE song = :1 AND when >= :2 '
-				'ORDER BY when DESC LIMIT %d' % limit,\
-				song, minWhen )
-			# NOTE: Why don't we order by score value here? Because GQL doesn't allow it..yeah. It's OK - we don't expect more than 10 results, so we'll manually sort them later
+				'WHERE song = :1 AND date = :2 '
+				'ORDER BY value DESC LIMIT %d' % limit,\
+				song, datetime.today().date() )
 		else:
 			# all time
 			results = db.GqlQuery('SELECT player, value FROM ScoreEntry '
 				'WHERE song = :1 '
-				'ORDER BY when DESC LIMIT %d' % limit,\
+				'ORDER BY value DESC LIMIT %d' % limit,\
 				song )
 
 		if song != '':
-			# sort them by score value
-			entries = sorted( results, key=ScoreEntry.get_value, reverse=True )
-			for entry in entries:
-				self.output( '%s,%d|' % (entry.player, entry.value) )
+			for result in results:
+				self.output( '%s,%d|' % (result.player, result.value ) )
 		else:
 			self.output('no song name given')
 
